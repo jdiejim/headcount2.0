ea7f86898fac8b9db458917a468d051308113a02
Juan Diego Jimenez <jdiejim@gmail.com>
2017-06-27 15:47:40 -0600

    add card comp

--- src/components/SchoolDataTable.js
+++ src/components/SchoolDataTable.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LineChart, Line } from 'recharts';
+// import { LineChart, Line } from 'recharts';
 import PropTypes, { shape, string, number } from 'prop-types';
 
 const SchoolDataTable = ({ data }) => {
@@ -16,14 +16,14 @@ const SchoolDataTable = ({ data }) => {
       e.low = data[e.name];
     }
   })
-  
+
   return(
     <div>
       School data
-      <LineChart width={150} height={50} data={schoolData}>
+      {/* <LineChart width={150} height={50} data={schoolData}>
         <Line type='linear' dataKey='up' stroke='blue' />
         <Line type='linear' dataKey='low' stroke='red' />
-      </LineChart>
+      </LineChart> */}
     </div>
   )
 }
