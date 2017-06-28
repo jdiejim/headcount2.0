aef3651b776a326663d535420b29c074b83b27e6
Jason Collins <the.oem@gmail.com>
2017-06-28 16:08:17 -0600

    WIP

--- src/components/ComparisonDataTable.js
+++ src/components/ComparisonDataTable.js
@@ -1,23 +1,43 @@
 import React from 'react';
-import TableCell  from './TableCell';
+import ComparisonTableCell  from './ComparisonTableCell';
 import { shape, objectOf, arrayOf, string, number, data, object } from 'prop-types';
 import './styles/ComparisonDataTable.css';
 
 const ComparisonDataTable = ({ schools }) => {
-console.log(schools);
+  if(schools.length === 0){
+    return (<div>Please select a school.</div>);
+  }
 
-  
+  let dataKeys = {};
+  schools.forEach((school) => {
+    dataKeys = Object.assign(dataKeys, school.data)
+  })
 
-  // const tableCells = Object.keys(data).map(e =>
-  //   <TableCell
-  //     key={Math.round(Date.now() * Math.random())}
-  //     data={data[e]}
-  //     year={e}
-  //   />);
+  const combinedData = Object.keys(dataKeys).reduce((obj, year) => {
+    if(!obj[year]) obj[year] = []
+
+    if(schools[0]){
+      if(!schools[0].data[year]) schools[0].data[year] = 0
+      obj[year].push(schools[0].data[year])
+    }
+    if(schools[1]){
+      if(!schools[1].data[year]) schools[1].data[year] = 0
+      obj[year].push(schools[1].data[year])
+    }
+    return obj;
+  }, {})
+
+  const comparisonTableCells = Object.keys(dataKeys).map(e => {
+    return <ComparisonTableCell
+      key={Math.round(Date.now() * Math.random())}
+      data={combinedData[e]}
+      year={e}
+    />
+  })
 
   return(
     <div className="comparison-table">
-      {/* {tableCells} */}
+      {comparisonTableCells}
     </div>
   )
 }
--- src/components/ComparisonTableCell.js
+++ src/components/ComparisonTableCell.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { string, number } from 'prop-types';
+import { string, number, arrayOf } from 'prop-types';
 import './styles/ComparisonTableCell.css';
 
 const ComparisonTableCell = ({ data, year }) => {
-  const dataStyle = {
-    backgroundColor: data > 0.5 ? '#6FD1BB' : '#E58B8B'
-  }
+  console.log('data', data);
+
+  data.map(e => e > 0.5 ? "<span className='cell-blue'>{e}</span>" : "<span className='cell-red'>{e}</span>")
+
+  console.log('data', data);
+
+  // const dataStyle = {
+  //   backgroundColor: data > 0.5 ? '#6FD1BB' : '#E58B8B'
+  // }
 
   return (
     <article className="table-cell">
-      <p className="year-cell">{year}</p>
+      {/* <p className="year-cell">{year}</p>
       <div className="data-cell">
         <p style={dataStyle}>{data}</p>
-      </div>
+      </div> */}
     </article>
   )
 }
 
 ComparisonTableCell.propTypes = {
-  data: number,
+  data: arrayOf(number),
   year: string
 }
 
